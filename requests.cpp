f3440c8a7b8f65bdb2f7e0bf62af957ac53a6b32
David <david.azoherra@gmail.com>
2019-06-10 23:15:17 -0600

    se anaden modulos de comunicacion con servidor

---
+++ requests.cpp
@@ -0,0 +1,69 @@
+//
+// Created by david on 07/06/19.
+//
+
+#include "requests.h"
+
+Requests::Requests(std::string &ipAddress, std::string &port) {
+    this->ipAddress = &ipAddress;
+    this->port = &port;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+}
+
+size_t Requests::WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+std::string Requests::sendPostRequest(std::string &data, ResourceOfRequest resourceOfRequest) {
+
+    this->readBuffer = "";
+    std::string request;
+
+    switch (resourceOfRequest) {
+        case INSERT:
+            request = "INSERT";
+            break;
+        case SELECT:
+            request = "SELECT";
+            break;
+        case UPDATE:
+            request = "UPDATE";
+            break;
+        case DELETE:
+            request = "DELETE";
+            break;
+    }
+
+    // Generate url with specified parameters
+    std::ostringstream url;
+    url << "http://" << *this->ipAddress << ":" << *this->port << "/" << request;
+
+    this->curl = curl_easy_init();
+    if(this->curl) {
+        // set up curl to fulfill post request with parameters
+        curl_easy_setopt(curl, CURLOPT_URL, url.str().c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, data.length());
+        curl_easy_setopt(curl, CURLOPT_POST, 1);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &this->readBuffer);
+
+        // Perform the request, res will get the return code
+        this->res = curl_easy_perform(this->curl);
+        std::cout << this->readBuffer << std::endl;
+        /* Check for errors */
+        if(this->res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(this->res));
+
+        // cleanup for next request
+        curl_easy_cleanup(this->curl);
+
+    }
+    curl_global_cleanup();
+
+    return this->readBuffer;
+}
---
+++ requests.h
@@ -0,0 +1,53 @@
+//
+// Created by david on 07/06/19.
+//
+
+#ifndef INVINCIBLE_CLIENTLOGIC_REQUESTS_H
+#define INVINCIBLE_CLIENTLOGIC_REQUESTS_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <curl/curl.h>
+#include "resourceofrequest.h"
+
+class Requests {
+public:
+
+    /**
+     * Obtiene ipaddress y puerto a donde hacer el request.
+     * Tambien, en caso de correrse en windows inicia winsock
+     */
+    Requests(std::string &ipAddress, std::string &port);
+
+    /**
+     * Permite que curl pueda escribir en el readBuffer la resquesta que el servidor da al hacer un request.
+     *
+     * @param contents
+     * @param size
+     * @param nmemb
+     * @param userp
+     * @return
+     */
+    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+
+    /**
+     * Envia un POST request.
+     *
+     * @param data, info que se enviara al servidor como cuerpo del request
+     * @param resourceOfRequest, tipo de REST resource al cual se le quiere hacer el request
+     */
+    std::string sendPostRequest(std::string &data, ResourceOfRequest resourceOfRequest);
+
+private:
+    CURL *curl;
+    // Si es puntero, da error de sigsegv por como curl lo maneja
+    CURLcode res;
+
+    std::string readBuffer;
+    std::string *ipAddress;
+    std::string *port;
+};
+
+
+#endif //INVINCIBLE_CLIENTLOGIC_REQUESTS_H
